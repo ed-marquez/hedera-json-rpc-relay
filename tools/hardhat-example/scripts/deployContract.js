02a96e88d3858a34bc21b77884635ba5504defd4
ed-marquez <72571340+ed-marquez@users.noreply.github.com>
2023-02-18 00:07:55 -0500

    Clean up to focuse on token creation and transfer.

--- tools/hardhat-example/scripts/deployContract.js
+++ tools/hardhat-example/scripts/deployContract.js
@@ -1,39 +1,19 @@
-/*-
- *
- * Hedera JSON RPC Relay - Hardhat Example
- *
- * Copyright (C) 2022 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 const hre = require('hardhat');
 const hethers = require('@hashgraph/hethers');
 
 module.exports = async () => {
   const provider = new hre.ethers.providers.JsonRpcProvider(process.env.RELAY_ENDPOINT);
   const wallet = new hre.ethers.Wallet(process.env.OPERATOR_PRIVATE_KEY, provider);
-  // const Greeter = await hre.ethers.getContractFactory('Greeter', wallet);
-  // const greeter = await Greeter.deploy('initial_msg');
-  const Greeter = await hre.ethers.getContractFactory('TokenCreateContract', wallet);
-  const greeter = await Greeter.deploy();
-  const contractAddress = (await greeter.deployTransaction.wait()).contractAddress;
+  
+  const payableAmount = hre.ethers.utils.parseEther("15.1")
+  const gasLimit = 4000000;
 
-  // const contractId = hethers.utils.getAccountFromAddress(contractAddress);
-  // console.log(`Greeter contract id: ${hethers.utils.asAccountString(contractId)}`);
+  const contract = await hre.ethers.getContractFactory('TokenCreateContract', wallet);
+  const tokenFacet = await contract.deploy({value: payableAmount, gasLimit});
+  
+  const contractAddress = (await tokenFacet.deployTransaction.wait()).contractAddress;
 
-  console.log(`Greeter deployed to: ${contractAddress}`);
+  console.log(`- Deployed contract (token faucet) to: ${contractAddress}`);
 
   return contractAddress;
 };
--- tools/hardhat-example/scripts/transferTokens.js
+++ tools/hardhat-example/scripts/transferTokens.js
@@ -1,37 +1,16 @@
-/*-
- *
- * Hedera JSON RPC Relay - Hardhat Example
- *
- * Copyright (C) 2022 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 const hre = require('hardhat');
 const hethers = require('@hashgraph/hethers');
 
-module.exports = async (cAddress) => {
+module.exports = async (contractAddress) => {
   const provider = new hre.ethers.providers.JsonRpcProvider(process.env.RELAY_ENDPOINT);
   const wallet = new hre.ethers.Wallet(process.env.OPERATOR_PRIVATE_KEY, provider);
 
-  // token = hethers.utils.getAddressFromAccount(tAddress);
+  const payableAmount = hre.ethers.utils.parseEther("5.1")
+  
+  const tokenSender = await hre.ethers.getContractAt('TokenCreateContract', contractAddress, wallet);
+  const tokenSendTx = await tokenSender.claimFT( {value: payableAmount } );
 
-  const tokenSender = await hre.ethers.getContractAt('TokenCreateContract', cAddress, wallet);
-  const tokenSendTx = await tokenSender.transferFT();
-  // const tokenSendRx = await tokenSendTx.wait();
-
-  console.log(`Transferred tokens`);
+  console.log(`\n- Transferred tokens`);
 
 
 };
